0f17910b878741e97cc0c25d2d90ccd0cfc3e23a
DiegoRod24 <99710581+DiegoRod24@users.noreply.github.com>
2026-09-16 02:56:38 -0500

    style: load order board interface

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { startSync } from './lib/store'
 import './styles.css'
+import './board.css'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js').catch(() => {}))
 }
 
-function SyncedApp() {
-  const [version, setVersion] = useState(0)
-
-  useEffect(() => startSync({
-    onRemote: () => setVersion(v => v + 1)
-  }), [])
-
-  return <App key={version} />
-}
-
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode><SyncedApp /></React.StrictMode>
+  <React.StrictMode><App /></React.StrictMode>
 )
